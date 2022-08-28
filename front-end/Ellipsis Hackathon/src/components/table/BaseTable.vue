dfd7d09168fb1d4690786d7bb8eb5c0deff227c7
RetxTan <74506417+RetxTan@users.noreply.github.com>
2022-08-28 11:19:25 +0800

    Eric laoban heng heng
    I love ellipsis

--- front-end/Ellipsis Hackathon/src/components/table/BaseTable.vue
+++ front-end/Ellipsis Hackathon/src/components/table/BaseTable.vue
@@ -1,44 +1,12 @@
-// Take in 2 props (Table header : string array, Table data : Object array)
-// const strAryExp = ["hello", "world"];
-// const products = [{ 
-//     property1: "value of prop1",
-//     property2: "value2",
-//     property3: "value3"
-//  }];
 <script>
 export default {
-  data() {
-    return {
-      finance_requests: [],
-    };
-  },
-  mounted() {
-    fetch("http://127.0.0.1:5001/finance_request/2", {
-      method: "GET",
-      headers: { "Content-Type": "application/json" },
-    })
-      .then(async (res) => {
-        const { finance_requests } = await res.json(); // const finance_request = await res.json().finance_request
-        this.finance_requests = finance_requests;
-      })
-      .catch((err) => console.log(err));
-  },
-};
+  props: {
+    tableName: String,
+    tableHeader: Array,
+    tableData: Array,
+  }
+}
 </script>
-
-
-
-<script setup>
-const table = [
-  { header: "Interest Rate", field: "interest_rate" },
-  { header: "Amount Requested", field: "request_amount" },
-
-  { header: "Debt Collection/Returned", field: "paid_amount" },
-  { header: "Status of Loan", field: "status" },
-];
-</script>
-
-
 
 <template>
   <div class="center">
@@ -47,15 +15,14 @@ const table = [
         <h1>DataTable</h1>
         <p>DataTable displays data in tabular format.</p>
       </div>
-      <AppDemoActions />
     </div>
 
     <div class="content-section implementation">
       <div class="card">
-        <DataTable :value="finance_requests">
-          <h5>Your Mum</h5>
+        <DataTable :value="tableData">
+          <h5>{{tableName}}</h5>
           <Column
-            v-for="col of table"
+            v-for="col of tableHeader"
             :field="col.field"
             :header="col.header"
             :key="col.field"
@@ -64,47 +31,5 @@ const table = [
         </DataTable>
       </div>
     </div>
-
-    <DataTableDoc />
   </div>
 </template>
-
-<style lang="scss" scoped>
-::v-deep(.p-paginator) {
-  .p-paginator-current {
-    margin-left: auto;
-  }
-}
-::v-deep(.p-progressbar) {
-  height: 0.5rem;
-  background-color: #d8dadc;
-  .p-progressbar-value {
-    background-color: #607d8b;
-  }
-}
-::v-deep(.p-datepicker) {
-  min-width: 25rem;
-  td {
-    font-weight: 400;
-  }
-}
-::v-deep(.p-datatable.p-datatable-customers) {
-  .p-datatable-header {
-    padding: 1rem;
-    text-align: left;
-    font-size: 1.5rem;
-  }
-  .p-paginator {
-    padding: 1rem;
-  }
-  .p-datatable-thead > tr > th {
-    text-align: left;
-  }
-  .p-datatable-tbody > tr > td {
-    cursor: auto;
-  }
-  .p-dropdown-label:not(.p-placeholder) {
-    text-transform: uppercase;
-  }
-}
-</style>
---
+++ front-end/Ellipsis Hackathon/src/pages/admin/AdminListFinancingReq.vue
@@ -0,0 +1,52 @@
+<script>
+</script>
+
+
+<script>
+import BaseTable from "../../components/table/BaseTable.vue";
+
+export default {
+  //   props: {
+
+  //   },
+  data() {
+    return {
+      finance_requests: [],
+      tableHeader: [
+        { header: "Interest Rate", field: "interest_rate" },
+        { header: "Amount Requested", field: "request_amount" },
+        { header: "Debt Collection/Returned", field: "paid_amount" },
+        { header: "Status of Loan", field: "status" },
+      ],
+    };
+  },
+  mounted() {
+    fetch("http://127.0.0.1:5001/finance_request/2", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then(async (res) => {
+        const { finance_requests } = await res.json(); // const finance_request = await res.json().finance_request
+        this.finance_requests = finance_requests;
+      })
+      .catch((err) => console.log(err));
+  },
+  components: {
+    BaseTable,
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>Overview of all Financing Request (Admin)</h1>
+    <BaseTable
+      tableName="Financing Requests (Admin)"
+      :tableHeader="tableHeader"
+      :tableData="finance_requests"
+    />
+  </div>
+</template>
+
+<style scoped>
+</style>
