6dc9f900c9cdd7dff8f151a4ba5a2c17be4f7f85
Bk49 <ericngyongwei@gmail.com>
2022-08-28 09:09:38 +0800

    Resolve Merge Conflicts

--- front-end/Ellipsis Hackathon/src/pages/shared/Login.vue
+++ front-end/Ellipsis Hackathon/src/pages/shared/Login.vue
@@ -1,36 +1,38 @@
 <script>
 export default {
-        methods: {
-            async authenticate() {
-                // try {
-                //     const requestOptions = {
-                //         method: "POST",
-                //         headers: { "Content-Type": "application/json" },
-                //         body: JSON.stringify({ username: this.username, password: this.password })
-                //     };
-                //     let response = await fetch("http://127.0.0.1:5001/authenticate", requestOptions);
-                //     console.log(response.status);
+    methods: {
+        async authenticate() {
+            try {
+                const requestOptions = {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({
+                        username: this.username,
+                        password: this.password,
+                    }),
+                };
+                const response = await fetch(
+                    "http://127.0.0.1:5001/authenticate",
+                    requestOptions
+                ).then((res) => res.json());
 
-                // } catch (error) {
-                //     console.log(error);
-                // }
-
-                try {
-                    const requestOptions = {
-                        method: "GET",
-                        headers: { "Content-Type": "application/json" },
-                    };
-                    let response = await fetch("http://127.0.0.1:5001/user", requestOptions);
-                    const data = await response.json();
-
-                    console.log(data.users)
-
-                } catch (error) {
-                    console.log(error);
+                if (response.role) {
+                    this.$router.push(
+                        `${
+                            response.role === "admin"
+                                ? "/admin-overview-financing-request"
+                                : "/client-overview-financing-req"
+                        }`
+                    );
+                } else {
+                    alert("The user details are not valid");
                 }
+            } catch (error) {
+                alert("Invalid request!");
             }
-        }
-    }
+        },
+    },
+};
 </script>
 
 <template>
@@ -38,11 +40,15 @@ export default {
         <img src="../../assets/logo.png" />
         <div id="login" class="center2">
             <span class="p-float-label">
-                <InputText id="username" type="text" v-model="username" placeholder="Username"/>
+                <InputText
+                    id="username"
+                    type="text"
+                    v-model="username"
+                    placeholder="Username"
+                />
             </span>
 
-            <span
-                >{{ value1 }}
+            <span>
                 <InputText
                     name="password"
                     type="password"
@@ -51,10 +57,8 @@ export default {
                 />
             </span>
 
-            <div class = "center" style = "top: 120%">
-                <router-link to="/edit-client">
-                    <Button label="Login" v-on:click="authenticate()"></Button>
-                </router-link>
+            <div class="center" style="top: 120%">
+                <Button @click="authenticate()" label="Login"></Button>
             </div>
         </div>
     </div>
