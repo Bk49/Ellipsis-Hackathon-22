084015d964429e0feb06f27f331a14bee5949593
RetxTan <74506417+RetxTan@users.noreply.github.com>
2022-08-28 15:46:39 +0800

    hi yoza

--- front-end/Ellipsis Hackathon/src/components/navigations/NavBar.vue
+++ front-end/Ellipsis Hackathon/src/components/navigations/NavBar.vue
@@ -1,48 +1,44 @@
 <template>
   <div>
-	<nav>
-	<router-link :to="{name: name}"></router-link>
     <Menubar :model="items">
-      <template #start>
-        <img
-          alt="logo"
-          src="https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png"
-          height="40"
-          class="mr-2"
-        />
-      </template>
+    <template #item="{item}">
+        <router-link :to="item.to" custom v-slot="{href, route, navigate, isActive, isExactActive}">
+            <a :href="href" @click="navigate" :class="{'active-link': isActive, 'active-link-exact': isExactActive}">{{route.fullPath}}
+				item.labels
+			</a>
+        </router-link>
+    </template>
     </Menubar>
-	</nav>
   </div>
 </template>
 
 <script>
 export default {
-			data() {
-				return {
-					items: [
-						{
-							name: "Financing Request List - Admin",
-							label:'Financing Request List - Admin',
-							path: "/admin-overview-financing-request",
-						},
-						{
-							name: "Client List",
-							label:'Client List',
-							path: "/list-client",
-						},
-						{
-							name: "Financing Request List - Client",
-							label:'Financing Request List - Client',
-							path: "/client-overview-financing-request",
-				        },
-                        {
-							name: "Update Financing Request",
-							label:'Update Financing Request',
-							path: "/view-financing-request",
-				        },
-                    ]
-			    }
-		    }
-        }
+  data() {
+    return {
+      items: [
+        {
+          name: "Financing Request List - Admin",
+          label: "Financing Request List - Admin",
+          to: "/admin-overview-financing-request",
+        },
+        {
+          name: "Client List",
+          label: "Client List",
+          to: "/list-client",
+        },
+        {
+          name: "Financing Request List - Client",
+          label: "Financing Request List - Client",
+          to: "/client-overview-financing-request",
+        },
+        {
+          name: "Update Financing Request",
+          label: "Update Financing Request",
+          to: "/view-financing-request",
+        },
+      ],
+    };
+  },
+};
 </script>
